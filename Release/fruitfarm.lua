f463aeb000bb985a250b6e33ca12f35ae358cc70
VNT-UNIVERSAL <vlr.ctcp@gmail.com>
2024-08-09 22:58:50 +0700

    Update fruitfarm.lua

--- Release/fruitfarm.lua
+++ Release/fruitfarm.lua
@@ -4,8 +4,5 @@ if queueteleport then
 local script = [[loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Panda-Hub/main/Release/fruit.lua'))()]]
 queueteleport(script)
 end
-if not getgenv().WebhookUrl then
-getgenv().WebhookUrl = 'https://discord.com/api/webhooks/1271493673246396438/sPm0l9pa9FvXB7DOoJirPKKLg9_0NGXF6Hq80MAeh_CcGmuYB91gj'
-end
 loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Panda-Hub/main/Release/fruit.lua'))()
 end
