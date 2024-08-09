88c5809f86fc7fd146fc3c66da3c7ef81359e24f
VNT-UNIVERSAL <vlr.ctcp@gmail.com>
2024-08-09 22:28:16 +0700

    Create fruitfarm.lua

---
+++ Release/fruitfarm.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+if queueteleport then
+local script = [[loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Panda-Hub/main/Release/fruit.lua'))()]]
+queueteleport(script)
+end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Panda-Hub/main/Release/fruit.lua'))()
+end
